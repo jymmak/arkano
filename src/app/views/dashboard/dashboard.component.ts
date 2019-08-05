940c8a45e61890a119f4e7c6fbe187d233b5e0d8
jymmak <jkmogollon.cp@gmail.com>
2019-08-05 14:51:00 -0400

    get listData

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -9,45 +9,39 @@ declare var $: any;
     selector: 'dashboard',
     templateUrl: 'dashboard.template.html'
 })
-export class dashboardComponent implements OnInit, AfterViewInit  {
+export class dashboardComponent implements OnInit, AfterViewInit {
     public title: string
     public subtitle: string
     public listCards: Array<any>;
     public dashboardForm: FormGroup;
-   
+    public cantidadCompras: any;
+    public incrementoCompras: any;
+    public cantidadNuevosUsuarios: any;
+    public cantidadNuevasVisitas: any;
+    public listData: any;
 
     constructor(private router: Router) {
+        this.listData = [];
 
-    
     }
     initForm() {
-        this.dashboardForm= new FormGroup({
+        this.dashboardForm = new FormGroup({
             cantidadCompras: new FormControl("", [Validators.required]),
             incrementoCompras: new FormControl("", [Validators.required]),
             cantidadNuevosUsuarios: new FormControl("", [Validators.required]),
             cantidadNuevasVisitas: new FormControl("", [Validators.required])
-            
+
         });
 
     }
-    dashboard(){
-        $("#dashboardModal").modal("hide");
-        let data: any = {
-            cantidadCompras: this.dashboardForm.get('cantidadCompras').value,
-            incrementoCompras: this.dashboardForm.get('incrementoCompras').value,
-            cantidadNuevosUsuarios: this.dashboardForm.get('cantidadNuevosUsuarios').value,
-            cantidadNuevasVisitas: this.dashboardForm.get('cantidadNuevasVisitas').value
-        };
-        console.log(data)
-    }
+
     ngOnInit() {
-   
         this.initForm();
         this.title = 'Dashboard';
         this.subtitle = 'Estadísticas';
         this.listCards = [
             {
-                id:1,
+                id: 1,
                 icon: 'fa fa-shopping-bag',
                 count: 150,
                 desc: 'Nuevas Compras',
@@ -57,7 +51,7 @@ export class dashboardComponent implements OnInit, AfterViewInit  {
                 moreColor: '#258fd7'
             },
             {
-                id:2,
+                id: 2,
                 icon: 'fa fa-bar-chart',
                 count: '53%',
                 desc: 'Incremento de Compras',
@@ -67,7 +61,7 @@ export class dashboardComponent implements OnInit, AfterViewInit  {
                 moreColor: '#0d730d'
             },
             {
-                id:3,
+                id: 3,
                 icon: 'fa fa-user-plus',
                 count: 44,
                 desc: 'Nuevos Usuarios',
@@ -77,7 +71,7 @@ export class dashboardComponent implements OnInit, AfterViewInit  {
                 moreColor: '#B27D0B'
             },
             {
-                id:4,
+                id: 4,
                 icon: 'fa fa-pie-chart',
                 count: 65,
                 desc: 'Nuevas Visitas',
@@ -90,13 +84,31 @@ export class dashboardComponent implements OnInit, AfterViewInit  {
 
 
 
-
-
-
     }
 
+
+    dashboard() {
+        $("#dashboardModal").modal("hide");
+        // let data: any = {
+        //     cantidadCompras: this.dashboardForm.get('cantidadCompras').value,
+        //     incrementoCompras: this.dashboardForm.get('incrementoCompras').value,
+        //     cantidadNuevosUsuarios: this.dashboardForm.get('cantidadNuevosUsuarios').value,
+        //     cantidadNuevasVisitas: this.dashboardForm.get('cantidadNuevasVisitas').value
+        // };
+        // console.log(data);
+        this.listData = {
+            cantidadCompras: this.cantidadCompras,
+            incrementoCompras: this.incrementoCompras,
+            cantidadNuevosUsuarios: this.cantidadNuevosUsuarios,
+            cantidadNuevasVisitas: this.cantidadNuevasVisitas
+
+        }
+        console.log(this.listData);
+    }
+
+
     ngAfterViewInit() {
-      
+
     }
 
 }
